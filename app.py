d3cf38876230ca6755bc644b9c9dca7c94fb14ae
Barrett Morrissette <bmorrissette34@gmail.com>
2024-04-30 14:27:23 -0500

    added header and vehicles dataset

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import plotly.express as pex
 import altair as al
 import pandas as pd
 
+st.header('Vehicle Sales US Data')
+
+st.data('/Users/barrett22/Desktop/Sprint_4_Project/Sprint4Project/vehicles_us.csv')
